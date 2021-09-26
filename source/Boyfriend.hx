95e6a33903f4422ce125bcdf1329290401981fc4
ShiroYT09 <91398537+ShiroYT09@users.noreply.github.com>
2021-09-25 21:45:21 -0400

    Update Boyfriend.hx

--- source/Boyfriend.hx
+++ source/Boyfriend.hx
@@ -1,9 +1,11 @@
 package;
-
-import flixel.FlxG;
-import flixel.FlxSprite;
-import flixel.graphics.frames.FlxAtlasFrames;
-import flixel.util.FlxTimer;
+[
+    [
+        "sonicbf\/BoyFriend_Assets.png",
+        "sonicbf\/BoyFriend_Assets.xml",
+        "sonicbf\/iconGrid.png"
+    ]
+]
 
 using StringTools;
 
